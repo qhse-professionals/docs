e0e5feb9a0a85e70d1c4c79cc12b53de88929bcf
Mike van den Berge <mike.van.den.berge@gmail.com>
2024-12-03 13:13:26 +0100

    Changes to main page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,20 +5,20 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'QHSE Professionals',
+  tagline: 'Welkom bij QHSE-Professionals, het werving & selectie bureau voor vaste banen en interim functies op het gebied van Quality, Health, Safety & Environment.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.qhse-professionals.nl',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'qhse-professionals', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
